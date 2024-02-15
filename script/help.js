ae449e99f6bd115aa615218d54c9ab12b48e3038
garfieldwu1 <152994099+garfieldwu1@users.noreply.github.com>
2024-02-15 23:11:19 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,10 +25,9 @@ module.exports.run = async function({
 			let page = 1;
 			let start = (page - 1) * pages;
 			let end = start + pages;
-			let helpMessage = `====『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧: 』====\n
-▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n`;
+			let helpMessage = `𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
 			for (let i = start; i < Math.min(end, commands.length); i++) {
-				helpMessage += `╭─╮\n |\t『 ${i + 1}.』  ${prefix}${commands[i]}\n╰─────────────ꔪ\n`;
+				helpMessage += `\t${i + 1}.  ${prefix}${commands[i]}\n`;
 			}
 			helpMessage += '\n====『 𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦T: 』====\n▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n';
 			eventCommands.forEach((eventCommand, index) => {
