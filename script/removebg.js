510a24d154f529abe0ae682c95bbd15a3689a073
garfieldwu1 <152994099+garfieldwu1@users.noreply.github.com>
2024-02-27 12:17:38 +0800

    Update removebg.js

--- script/removebg.js
+++ script/removebg.js
@@ -3,56 +3,36 @@ const fs = require('fs-extra');
 
 module.exports.config = {
   name: "removebg",
-  version: "2.7",
-  hasPermission: 0,
+  version: "1.0.",
+  hasPermssion: 0,
   credits: "Mark Hitsuraan",
-  description: "Remove background",
-  commandCategory: "no prefix",
-  usages: "photo without background",
-  cooldown: 3,
+  description: "enhance your photo ",
+  usePrefix: true,
+  commandCategory: "image",
+  usages: "< reply image >",
+  cooldowns: 2,
 };
 
-module.exports.handleEvent = async function ({ api, event }) {
-  if (!(event.body.indexOf("removebg") === 0 || event.body.indexOf("Removebg") === 0)) return;
-  const args = event.body.split(/\s+/);
-  args.shift();
+module.exports.run = async ({ api, event, args }) => {
+  let pathie = __dirname + `/cache/removed_bg.jpg``;
+  const { threadID, messageID } = event;
+
+  var mark = event.messageReply ? event.messageReply.attachments[0].url : args.join(" ");
 
   try {
-    const response = await axios.get("https://allinoneapis.onrender.com");
-    if (response.data.hasOwnProperty("error")) {
-      return api.sendMessage(response.data.error, event.threadID, event.messageID);
-    }
+    api.sendMessage("Removing background...", threadID, messageID);
+    const response = await axios.get(`https://allinoneapis.onrender.com/api/try/removebg?url=${encodeURIComponent(mark)}`);
+    const processedImageURL = response.data.image_data;
 
-    let pathie = __dirname + `/cache/removed_bg.jpg`;
-    const { threadID, messageID } = event;
+    const img = (await axios.get(processedImageURL, { responseType: "arraybuffer"})).data;
 
-    let photoUrl = event.messageReply ? event.messageReply.attachments[0].url : args.join(" ");
+    fs.writeFileSync(pathie, Buffer.from(img, 'utf-8'));
 
-    if (!photoUrl) {
-      api.sendMessage("Please reply to photo.", threadID, messageID);
-      return;
-    }
-
-    api.sendMessage("Removing Background, Please wait...", threadID, async () => {
-      try {
-        const response = await axios.get(`https://allinoneapis.onrender.com/api/try/removebg?url=${encodeURIComponent(photoUrl)}`);
-        const processedImageURL = response.data.image_data;
-
-        const img = (await axios.get(processedImageURL, { responseType: "arraybuffer" })).data;
-
-        fs.writeFileSync(pathie, Buffer.from(img, 'binary'));
-
-        api.sendMessage({
-          body: "Here's your image without background.",
-          attachment: fs.createReadStream(pathie)
-        }, threadID, () => fs.unlinkSync(pathie), messageID);
-      } catch (error) {
-        api.sendMessage(`error: ${error}`, threadID, messageID);
-      }
-    });
+    api.sendMessage({
+      body: "Processed Image",
+      attachment: fs.createReadStream(pathie)
+    }, threadID, () => fs.unlinkSync(pathie), messageID);
   } catch (error) {
-    api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
-  }
+    api.sendMessage(`Error processing image: ${error}`, threadID, messageID);
+  };
 };
-
-module.exports.run = async function ({ api, event }) {};
