ca603b50ff2fc026b31a91eebe0ca5a9e352e037
garfieldwu1 <152994099+garfieldwu1@users.noreply.github.com>
2024-02-27 12:24:42 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,42 +1,32 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const config = {
-  name: "ai",
-  version: "1.0.0",
-  hasPermission: 0,
-  credits: "unknown",
-  description: "OpenAI official AI with no prefix",
-  commandCategory: "education",
-  usePrefix: true,
-  usages: "...",
-  cooldowns: 0
+module.exports.config = {
+    name: "ai",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "Jonell Magallanes", //API BY MARK
+    description: "EDUCATIONAL",
+    usePrefix: true,
+    commandCategory: "AI",
+    usages: "[question]",
+    cooldowns: 10
 };
 
-const handleEvent = async function ({ api, event, client, __GLOBAL }) {
+module.exports.run = async function ({ api, event, args }) {
+    const content = encodeURIComponent(args.join(" "));
+    const apiUrl = `https://garfieldgpt4.onrender.com/api/gpt4?query=${content}`;
 
-  if (event.body.indexOf("ai") === 0 || event.body.indexOf("Ai") === 0) {
-    const { threadID, messageID } = event;
-    const input = event.body;
-    const message = input.split(" ");
+    if (!content) return api.sendMessage("Please provide a question first.", event.threadID, event.messageID);
 
-    if (message.length < 2) {
-      api.sendMessage("Please provide a question first.", event.threadID, event.messageID);
-    } else {
-      try {
-        api.sendMessage('Please bear with me while I ponder your request...', event.threadID, event.messageID);
-        const ris = await axios.get(`https://garfieldgpt4.onrender.com/api/gpt4?query=${message.slice(1).join(" ")}`);
-        const result = ris.data.Mark;
-        const Mark = `𝗔𝗜 🚀:\n\n${result}`;
-        api.sendMessage(Mark, event.threadID, event.messageID);
-      } catch (err) {
-        console.error(err);
-        api.sendMessage("Opsss! nawala yung cignal, Please try again.", event.threadID, event.messageID);
-      }
+    try {
+        api.sendMessage("Please bear with me while I ponder your request...", event.threadID, event.messageID);
+
+        const response = await axios.get(apiUrl);
+        const { Mark } = response.data;
+
+        api.sendMessage(`𝗔𝗜 🚀:\n\n${Mark}`, event.threadID, event.messageID);
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("An error occurred while processing your request.", event.threadID);
     }
-  }
 };
-
-const run = function ({ api, event, client, __GLOBAL }) {
-};
-
-module.exports = { config, handleEvent, run };
