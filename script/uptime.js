6c39d82e702e9a15b4923b84d0774c576a3de23c
garfieldwu1 <152994099+garfieldwu1@users.noreply.github.com>
2024-03-03 03:02:38 +0800

    Update uptime.js

--- script/uptime.js
+++ script/uptime.js
@@ -59,7 +59,7 @@ module.exports.run = async ({ api, event }) => {
 
 		const timeStart = Date.now();
 		const uptimeMessage = module.exports.getUptime(uptimeSeconds);
-		const returnResult = `BOT has been working for ${uptime}\n\n❖ Cpu usage: ${usage.cpu.toFixed(1)}%\n❖ RAM usage: ${module.exports.byte2mb(usage.memory)}\n❖ Cores: ${os.cpus().length}\n❖ Ping: ${Date.now() - timeStart}ms\n❖ Operating System Platform: ${osInfo.platform}\n❖ System CPU Architecture: ${osInfo.architecture}`;
+		const returnResult = `BOT has been working for ${uptimeSeconds}\n\n❖ Cpu usage: ${usage.cpu.toFixed(1)}%\n❖ RAM usage: ${module.exports.byte2mb(usage.memory)}\n❖ Cores: ${os.cpus().length}\n❖ Ping: ${Date.now() - timeStart}ms\n❖ Operating System Platform: ${osInfo.platform}\n❖ System CPU Architecture: ${osInfo.architecture}`;
 
 		await module.exports.saveStartTimestamp(startTime); // Save the start time again to ensure it's updated
 		return api.sendMessage(returnResult, event.threadID, event.messageID);
