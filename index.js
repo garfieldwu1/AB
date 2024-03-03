6768c2632540065f76d08b75aa595ac012565564
garfieldwu1 <152994099+garfieldwu1@users.noreply.github.com>
2024-03-03 09:44:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -709,7 +709,7 @@ function createConfig() {
 						admin: [],
 						devMode: false,
 						database: false,
-					        restartTime: 15
+					        restartTime: 999999999
 				},
 				fcaOption: {
 						forceLogin: true,
